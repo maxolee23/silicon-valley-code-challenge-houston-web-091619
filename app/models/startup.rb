fa816adb85d77fa50f8fe7dd2d66c63485b8406c
Max Lee <maxolee23@gmail.com>
2019-09-25 00:09:39 +0000

    Automatically backed up by Learn

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,35 +1,35 @@
 class Startup
 
-    attr_accessor :name 
-    attr_reader :founder, :domain 
+    attr_accessor :name
+    attr_reader :founder, :domain
 
     @@all = []
 
     def initialize(name, founder, domain)
-        @name = name 
-        @founder = founder 
-        @domain = domain 
-        @@all << self 
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
     end
 
-    def self.all 
-        @@all 
+    def self.all
+        @@all
     end
 
     def pivot(name, domain)
-        @name = name 
-        @domain = domain 
+        @name = name
+        @domain = domain
     end
 
     def self.find_by_founder(founder_name)
         self.all.find do |startup|
-            startup.founder == founder_name 
+            startup.founder == founder_name
         end
     end
 
-    def self.domains 
+    def self.domains
         self.all.map do |startup|
-            startup.domain 
+            startup.domain
         end
     end
 
@@ -37,41 +37,41 @@ class Startup
         FundingRound.new(self, vc, type, investment)
     end
 
-    def num_funding_rounds 
+    def num_funding_rounds
         count = 0
-        FundingRound.all.each do |fundinground| 
-            if fundinground.startup == self 
-                count += 1 
+        FundingRound.all.each do |fundinground|
+            if fundinground.startup == self
+                count += 1
             end
         end
         count
     end
 
-    def total_funds 
+    def total_funds
         total = 0
         FundingRound.all.each do |fundinground|
-            if fundinground.startup == self 
-                total += fundinground.investment 
+            if fundinground.startup == self
+                total += fundinground.investment
             end
         end
-        total 
+        total
     end
 
-    def fundingrounds 
+    def fundingrounds
         FundingRound.all.select do |fundinground|
-            fundinground.startup == self 
+            fundinground.startup == self
         end
     end
 
-    def investors 
+    def investors
         fundingrounds.map do |fundinground|
-            fundinground.venture_capitalist 
+            fundinground.venture_capitalist
         end
     end
 
-    def big_investors 
+    def big_investors
         self.investors().select do |investor|
-            VentureCapitalist.tres_commas_club().include?(investor)  
+            VentureCapitalist.tres_commas_club().include?(investor)
         end
     end
 
